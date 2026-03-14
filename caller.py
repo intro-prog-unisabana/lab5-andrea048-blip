377cbf0cbb9e81266f4852b2b4543231689a8153
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-03-14 14:19:54 -0500

    caller

--- caller.py
+++ caller.py
@@ -1,8 +1,10 @@
 from mystery_module import transform_data
 
 # FREEZE CODE BEGIN
-x = int(input())
-y = float(input())
+x =input()
+y =input()
+int(x)
+float(y)
 # FREEZE CODE END
 
 result = transform_data(x, y, "quiz_test")
