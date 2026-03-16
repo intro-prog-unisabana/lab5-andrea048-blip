f1b624eb9890dd56cfd4b06b71271dd65ca35818
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-03-16 07:15:20 -0500

    ...

--- mystery_module.py
+++ mystery_module.py
@@ -1,4 +1,4 @@
 # FREEZE CODE BEGIN
-def transform_data(x: int, y:float, z: str):
-    result =(x * y) / (len(z)) + 1 +sum(ord(c) for c in z) % 10
+def transform_data(x: int, y:float, z: str)-> float:
+    result =(x * y) / (len(z) + 1) +sum(ord(c) for c in z) % 10
     return round(result,2)
--- scope.py
+++ scope.py
@@ -1,15 +1,13 @@
 int_value = None
 str_value = None
-def set_globals(algun_int, algun_str):
+def set_globals(some_int, some_str):
     global int_value, str_value
-    int_value = algun_int
-    str_value = algun_str
+    int_value = some_int
+    str_value = some_str
 
 def get_globals():
     return (int_value, str_value)
 
-print(get_globals())
-set_globals(10,"Hello")
-print(get_globals())
+
 
 
