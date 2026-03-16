29fc78221868e299d89096bcd282cedc133e8625
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-03-16 07:07:47 -0500

    ..

--- mystery_module.py
+++ mystery_module.py
@@ -1,3 +1,4 @@
 # FREEZE CODE BEGIN
-def transform_data(x: int, y: float, z: str) -> float:
-    return x,y,z
+def transform_data(x: int, y:float, z: str):
+    result =(x * y) / (len(z)) + 1 +sum(ord(c) for c in z) % 10
+    return round(result,2)
--- return_types.py
+++ return_types.py
@@ -1,7 +1,4 @@
 def obtener_precio_usuario():
-    precio = input("Enter the item's price:\n")
-    return float(precio)
-precio = obtener_precio_usuario()
-print(precio)
-
+    precios = input("Enter the item's price:\n")
+    return float(precios)
  
